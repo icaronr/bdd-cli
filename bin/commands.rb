eb49b53a06e360c25334466d305bf3f171273130
Icaro Rezende <icaronr@gmail.com>
2021-07-21 14:31:50 -0300

    add cli and some commands

---
+++ bin/commands.rb
@@ -0,0 +1,30 @@
+module Behaviorism
+    def install
+        puts "Installing dependencies..."
+        puts "Generating initializer..."
+        puts "All done!"
+    end
+    
+    def analyse
+        puts "Running cucumber..."
+        begin
+        system "bundle exec cucumber", exception: true
+        rescue StandardError => err
+            puts "Error running cucumber..."
+            return
+        end
+        puts "Processing logs..."
+        puts "All done!"
+    end
+    
+    def clean
+        puts "Removing logs..."
+        puts "Removing factories..."
+        puts "All done!"
+    end
+    
+    def generate
+        puts "Checking command..."
+        puts "All done!"
+    end
+end
--- test_generator.gemspec
+++ test_generator.gemspec
@@ -16,4 +16,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rspec-rails', '>= 0'
   s.add_dependency 'shoulda-matchers', '~> 4.0'
   s.add_dependency 'factory_bot_rails'
+  s.executables = ['testgenerator']
 end
